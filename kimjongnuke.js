61e55c99f0bf482efd2f0c4a99f99e8111fdc36d
Benjamin Donahue <79435245+OblivionNoir@users.noreply.github.com>
2022-11-01 15:10:48 -0500

    Update kimjongnuke.js

--- kimjongnuke.js
+++ kimjongnuke.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function playanthem(){
-    var anthem = new Audio("anthem.mp3");
+    var anthem = new Audio("https://soundcloud.com/user-727553192/anthem/s-uq4yMbmG68k?in=user-727553192/sets/kim-jong-nuke-sfx/s-Y9V8D585J2C&si=a3ad7756549a4ab8b4031f4b7daf1b3d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing");
     anthem.play()
     anthem.loop = true;
     document.removeEventListener('click', playanthem);
@@ -7,7 +7,7 @@ document.addEventListener('click', function playanthem(){
 
 
 document.addEventListener("click", function boom(){
-    var audio = new Audio("explosion.wav");
+    var audio = new Audio("https://soundcloud.com/user-727553192/explosion/s-be560hwfwQN?in=user-727553192/sets/kim-jong-nuke-sfx/s-Y9V8D585J2C&si=a3ad7756549a4ab8b4031f4b7daf1b3d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing");
     audio.play();
     document.onclick = userClicked;
     function userClicked() {
