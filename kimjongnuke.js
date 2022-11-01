946e0d87ea7b2b250b643b793327f5a9f9e08813
Benjamin Donahue <79435245+OblivionNoir@users.noreply.github.com>
2022-11-01 15:27:01 -0500

    Update kimjongnuke.js

--- kimjongnuke.js
+++ kimjongnuke.js
@@ -7,7 +7,7 @@ document.addEventListener('click', function playanthem(){
 
 
 document.addEventListener("click", function boom(){
-    var audio = new Audio("https://soundcloud.com/user-727553192/explosion/s-be560hwfwQN?in=user-727553192/sets/kim-jong-nuke-sfx/s-Y9V8D585J2C&si=a3ad7756549a4ab8b4031f4b7daf1b3d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing");
+    var audio = new Audio("https://cf-media.sndcdn.com/WGqCMPAZWnJW.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vV0dxQ01QQVpXbkpXLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjY3MzM0MjI2fX19XX0_&Signature=FckznNWKnOWLisnZtOqJS53w9BvQcg~ZwfBTKzLKY-tOUcaxSjSsrl9LvBEquitK9Oh3OCUFVhJsz57bglzgqF-OWPk-o-Vwhfm2jC1s~MPMchrxZbLjlGhHEbU6MegKYdL2nedodfioog3TYWHjusxWY6mEBUr1EFBPSRLOXBWJutHf-0pdhUxCbCb-eveSwZdRyxO2j4XZBiB~h0B9fzzDS6v-hLpXqNgddlJkaZ0Y3khZPB7D5UdxaNBibhogaUsAwDFI6fAt-jsdOqyDP0SWr1nVtiDcYJzW4DaO1PhcpHeGhkoZ0-WOWPj7u6qeqVVjMsHqVd46aa4njnrbyQ__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ");
     audio.play();
     document.onclick = userClicked;
     function userClicked() {
